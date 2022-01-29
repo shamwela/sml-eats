d41f2eba1e888d646b0324a53ec7b193472539bf
Sha Mwe La <shamwela2002@gmail.com>
2022-01-29 19:35:58 +0630

    Improve

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
 import 'styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type item = {
+  name: string
+  quantity: number
+  price: number
+}
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [cart, setCart] = useState<item[] | undefined>(undefined)
+
+  const updateCart = (item: item) => {
+    setCart((previousCart) => {
+      if (!previousCart) {
+        previousCart = []
+      }
+
+      return [...previousCart, item]
+    })
+  }
+
+  return <Component {...pageProps} cart={cart} updateCart={updateCart} />
 }
 
 export default MyApp
--- pages/store/the-pizza-company/seafood-cocktail.tsx
+++ pages/store/the-pizza-company/seafood-cocktail.tsx
@@ -1,9 +1,46 @@
-import Link from 'next/link'
+import Image from 'next/image'
+import SeafoodCocktailImage from 'public/images/seafood-cocktail.jpeg'
 
-const SeafoodCocktail = () => {
+const SeafoodCocktail = ({
+  cart,
+  updateCart,
+}: {
+  cart: any
+  updateCart: any
+}) => {
   return (
     <>
       <h1>Seafood Cocktail</h1>
+      <div className='max-w-md'>
+        <Image
+          alt='Seafood Cocktail'
+          src={SeafoodCocktailImage}
+          placeholder='blur'
+        />
+      </div>
+      <button
+        onClick={() =>
+          updateCart({
+            name: 'Seafood Cocktail',
+            quantity: 1,
+            price: 20,
+          })
+        }
+      >
+        Add 1 to order
+      </button>
+
+      <h2>Cart</h2>
+      {!cart && <p>Your cart is empty.</p>}
+      {cart?.map(({ name, quantity, price }) => {
+        return (
+          <div key={name}>
+            <p>
+              {name} - {quantity} x ${price}
+            </p>
+          </div>
+        )
+      })}
     </>
   )
 }
