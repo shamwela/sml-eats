2572413a8fd74341a736130832769cf66f0a10fe
Sha Mwe La <shamwela2002@gmail.com>
2022-02-08 21:40:20 +0630

    Improve

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 import Head from 'components/Head'
 import Link from 'next/link'
