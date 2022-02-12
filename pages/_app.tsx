6b4925cbf33de3059b8f9e7c276e09bfa30fc1e5
Sha Mwe La <shamwela2002@gmail.com>
2022-02-12 20:58:47 +0630

    Improve cart

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import 'styles/globals.css'
 
+import { useEffect, useState } from 'react'
+
 import type { AppProps } from 'next/app'
 import BBQPorkDeluxeImage from 'public/images/bbq-pork-deluxe.jpeg'
 import type { CartItem } from 'types/cartItem'
@@ -12,7 +14,6 @@ import type { Restaurant } from 'types/restaurant'
 import SeafoodCocktailImage from 'public/images/seafood-cocktail.jpeg'
 import StarbucksImage from 'public/images/starbucks.webp'
 import ThePizzaCompanyImage from 'public/images/the-pizza-company.webp'
-import { useState } from 'react'
 
 const restaurants: Restaurant[] = [
   {
@@ -89,6 +90,15 @@ const categoryImageProperties = [
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [cart, setCart] = useState<CartItem[]>([])
+  const [totalQuantity, setTotalQuantity] = useState(0)
+
+  useEffect(() => {
+    const totalQuantity = cart.reduce(
+      (previousQuantity, cartItem) => previousQuantity + cartItem.quantity,
+      0
+    )
+    setTotalQuantity(totalQuantity)
+  }, [cart])
 
   const addItem = (newCartItem: CartItem) => {
     const newCart = [...cart, newCartItem]
@@ -124,8 +134,23 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           </Link>
           {cart.length > 0 && (
             <Link href='/cart'>
-              <a className='button fixed right-5 left-5 bottom-5 md:static'>
-                View order
+              <a>
+                <button className='fixed right-5 left-5 bottom-5 z-10 flex items-center gap-x-2 justify-center md:static'>
+                  <svg
+                    aria-hidden='true'
+                    focusable='false'
+                    viewBox='0 0 16 16'
+                    fill='currentColor'
+                    className='h-4 w-4'
+                  >
+                    <path
+                      fillRule='evenodd'
+                      clipRule='evenodd'
+                      d='M3.666 11.333h10.333l1.334-8h-11l-.267-2h-3.4v2h1.667l1.333 8zm1.333 3.334A1.333 1.333 0 105 12a1.333 1.333 0 000 2.667zm9.334-1.334a1.333 1.333 0 11-2.667 0 1.333 1.333 0 012.667 0z'
+                    ></path>
+                  </svg>
+                  Cart • {totalQuantity}
+                </button>
               </a>
             </Link>
           )}
