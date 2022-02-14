6e13c158420af398c1f928f3e3b9918b5936204d
Sha Mwe La <shamwela2002@gmail.com>
2022-02-14 18:18:44 +0630

    Improve close button

--- ui/Close.tsx
+++ ui/Close.tsx
@@ -7,7 +7,7 @@ const Close = () => {
     <button
       onClick={() => router.back()}
       aria-label='Close'
-      className='max-w-fit bg-transparent p-2'
+      className='max-w-fit rounded-full bg-light-primary p-2'
     >
       <svg
         width='24px'
