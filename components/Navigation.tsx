2e2ead82d2cc8f3cfa510e0869b28f3ab28fdf80
Sha Mwe La <shamwela2002@gmail.com>
2022-02-22 16:18:25 +0630

    Improve navigation style

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -32,44 +32,43 @@ const Navigation = ({ cart }: NavigationProps) => {
   }
 
   return (
-    <nav className='top-0 z-10 flex flex-wrap justify-center gap-6 bg-white py-4 dark:bg-gray-900 md:sticky'>
-      <Link href='/'>
-        <a className='text-4xl font-bold'>SML Eats</a>
-      </Link>
-
-      {pathname !== '/search' && (
-        <Link href='/search'>
-          <a>
-            <button>Search</button>
-          </a>
+    <nav className='top-0 z-10 bg-white py-4 dark:bg-gray-900 md:sticky'>
+      <div className='mx-auto flex max-w-4xl flex-wrap items-center gap-4'>
+        <Link href='/'>
+          <a className='text-xl font-bold md:text-3xl'>SML Eats</a>
         </Link>
-      )}
-
-      {showCartButton && (
-        <Link href='/cart'>
-          <a>
-            <button className='fixed right-5 left-5 bottom-5 z-10 flex items-center justify-center gap-x-2 md:static'>
-              {/* Cart icon */}
-              <svg
-                aria-hidden='true'
-                focusable='false'
-                viewBox='0 0 16 16'
-                fill='currentColor'
-                className='h-4 w-4'
-              >
-                <path
-                  fillRule='evenodd'
-                  clipRule='evenodd'
-                  d='M3.666 11.333h10.333l1.334-8h-11l-.267-2h-3.4v2h1.667l1.333 8zm1.333 3.334A1.333 1.333 0 105 12a1.333 1.333 0 000 2.667zm9.334-1.334a1.333 1.333 0 11-2.667 0 1.333 1.333 0 012.667 0z'
-                ></path>
-              </svg>
-              Cart • {totalQuantity}
-            </button>
-          </a>
-        </Link>
-      )}
-
-      <button onClick={toggleTheme}>Change theme</button>
+        {pathname !== '/search' && (
+          <Link href='/search'>
+            <a>
+              <button>Search</button>
+            </a>
+          </Link>
+        )}
+        {showCartButton && (
+          <Link href='/cart'>
+            <a>
+              <button className='fixed right-5 left-5 bottom-5 z-10 flex items-center justify-center gap-x-2 md:static'>
+                {/* Cart icon */}
+                <svg
+                  aria-hidden='true'
+                  focusable='false'
+                  viewBox='0 0 16 16'
+                  fill='currentColor'
+                  className='h-4 w-4'
+                >
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M3.666 11.333h10.333l1.334-8h-11l-.267-2h-3.4v2h1.667l1.333 8zm1.333 3.334A1.333 1.333 0 105 12a1.333 1.333 0 000 2.667zm9.334-1.334a1.333 1.333 0 11-2.667 0 1.333 1.333 0 012.667 0z'
+                  ></path>
+                </svg>
+                Cart • {totalQuantity}
+              </button>
+            </a>
+          </Link>
+        )}
+        <button onClick={toggleTheme}>Change theme</button>
+      </div>
     </nav>
   )
 }
