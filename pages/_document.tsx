43b1610c014e20d459f2d691bef18a85c423f535
Sha Mwe La <shamwela2002@gmail.com>
2022-03-17 23:27:31 +0630

    CustomDocument

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
+class CustomDocument extends Document {
   render() {
     return (
       <Html lang='en'>
@@ -14,4 +14,4 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument
+export default CustomDocument
