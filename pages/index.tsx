a3343c7adb35b8ebcbed8954d94a94da5c649380
Sha Mwe La <shamwela2002@gmail.com>
2022-03-17 16:51:54 +0630

    Improve

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ const Home = () => {
           return (
             <Link href={href} key={name}>
               <a>
-                <section className='flex h-20 rounded-lg bg-light-elevation p-4 dark:bg-dark-elevation'>
+                <section className='flex h-20 rounded-lg bg-light-elevation p-4 dark:bg-dark-elevation justify-center'>
                   <span className='self-center'>{name}</span>
                   <Image
                     alt={name}
