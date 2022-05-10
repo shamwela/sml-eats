b72d16ddf9d57b2c2a54424e5634662aa06a4a87
Sha Mwe La <shamwela2002@gmail.com>
2022-05-10 19:40:08 +0630

    Uninstall react-image-size

--- react-image-size.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'react-image-size' {
-  type Size = {
-    width: number
-    height: number
-  }
-  // eslint-disable-next-line no-unused-vars
-  const reactImageSize: (src: string ) => Promise<Size>
-  export default reactImageSize
-}
