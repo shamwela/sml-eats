90c88f53a980c35979ac3d0eba2988e2998ae9a2
Sha Mwe La <shamwela2002@gmail.com>
2022-05-11 21:38:35 +0630

    Improve seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,36 +3,35 @@ import { categories, inputs, options, items, restaurants } from './data'
 
 const prisma = new PrismaClient()
 
-const load = async () => {
+const deleteData = async () => {
+  await prisma.category.deleteMany()
+  await prisma.input.deleteMany()
+  await prisma.option.deleteMany()
+  await prisma.item.deleteMany()
+  await prisma.restaurant.deleteMany()
+}
+
+const resetAutoIncrement = async () => {
+  await prisma.$queryRaw`alter table Category auto_increment = 1`
+  await prisma.$queryRaw`alter table Input auto_increment = 1`
+  await prisma.$queryRaw`alter table Option auto_increment = 1`
+  await prisma.$queryRaw`alter table Item auto_increment = 1`
+  await prisma.$queryRaw`alter table Restaurant auto_increment = 1`
+}
+
+const seed = async () => {
+  await prisma.category.createMany({ data: categories })
+  await prisma.input.createMany({ data: inputs })
+  await prisma.option.createMany({ data: options })
+  await prisma.item.createMany({ data: items })
+  await prisma.restaurant.createMany({ data: restaurants })
+}
+
+const main = async () => {
   try {
-    await prisma.category.deleteMany()
-    await prisma.input.deleteMany()
-    await prisma.option.deleteMany()
-    await prisma.item.deleteMany()
-    await prisma.restaurant.deleteMany()
-
-    // Reset auto increment to 1
-    await prisma.$queryRaw`alter table Category auto_increment = 1`
-    await prisma.$queryRaw`alter table Input auto_increment = 1`
-    await prisma.$queryRaw`alter table Option auto_increment = 1`
-    await prisma.$queryRaw`alter table Item auto_increment = 1`
-    await prisma.$queryRaw`alter table Restaurant auto_increment = 1`
-
-    await prisma.category.createMany({
-      data: categories,
-    })
-    await prisma.input.createMany({
-      data: inputs,
-    })
-    await prisma.option.createMany({
-      data: options,
-    })
-    await prisma.item.createMany({
-      data: items,
-    })
-    await prisma.restaurant.createMany({
-      data: restaurants,
-    })
+    deleteData()
+    resetAutoIncrement()
+    seed()
   } catch (error) {
     console.error(error)
     process.exit(1)
@@ -40,5 +39,4 @@ const load = async () => {
     await prisma.$disconnect()
   }
 }
-
-load()
+main()
