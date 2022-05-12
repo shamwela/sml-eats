32745d058276db299940bed01330eedc90f02b6b
Sha Mwe La <shamwela2002@gmail.com>
2022-05-12 10:20:38 +0630

    Improve

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,35 +3,26 @@ import { categories, inputs, options, items, restaurants } from './data'
 
 const prisma = new PrismaClient()
 
-const deleteData = async () => {
+const main = async () => {
   await prisma.category.deleteMany()
   await prisma.input.deleteMany()
   await prisma.option.deleteMany()
   await prisma.item.deleteMany()
   await prisma.restaurant.deleteMany()
-}
 
-const resetAutoIncrement = async () => {
   await prisma.$queryRaw`alter table Category auto_increment = 1`
   await prisma.$queryRaw`alter table Input auto_increment = 1`
   await prisma.$queryRaw`alter table Option auto_increment = 1`
   await prisma.$queryRaw`alter table Item auto_increment = 1`
   await prisma.$queryRaw`alter table Restaurant auto_increment = 1`
-}
 
-const seed = async () => {
   await prisma.category.createMany({ data: categories })
   await prisma.input.createMany({ data: inputs })
   await prisma.option.createMany({ data: options })
   await prisma.item.createMany({ data: items })
   await prisma.restaurant.createMany({ data: restaurants })
-}
 
-const main = async () => {
   try {
-    deleteData()
-    resetAutoIncrement()
-    seed()
   } catch (error) {
     console.error(error)
     process.exit(1)
