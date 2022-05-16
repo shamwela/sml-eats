41b0a7f50b57ec43e453632f4fffa3cf55f966ff
Sha Mwe La <shamwela2002@gmail.com>
2022-05-16 19:39:41 +0630

    Improve

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,36 +2,23 @@ import Head from 'components/Head'
 import { useAuthenticationState } from 'utilities/firebase'
 import Spinner from 'components/Spinner'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 const Profile = () => {
   const [user, loading, error] = useAuthenticationState()
   const router = useRouter()
-
-  useEffect(() => {
-    if (!user) {
-      router.push('/')
-    }
-  }, [user, router])
+  if (!user) router.push('/')
+  if (error) alert(error.message)
 
   return (
     <>
       <Head title='Profile' />
-
-      {(() => {
-        if (user) {
-          return (
-            <>
-              <span>Name: {user.displayName}</span>
-              <span>Email: {user.email}</span>
-            </>
-          )
-        } else if (loading) {
-          return <Spinner />
-        } else if (error) {
-          alert(error)
-        }
-      })()}
+      {loading && <Spinner />}
+      {user && (
+        <>
+          <span>Name: {user.displayName}</span>
+          <span>Email: {user.email}</span>
+        </>
+      )}
     </>
   )
 }
