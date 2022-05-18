adc28fae8b4f01562ea08a4aa849217553a121fd
Sha Mwe La <shamwela2002@gmail.com>
2022-05-18 16:09:01 +0630

    Fix router error

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,13 +2,18 @@ import Head from 'components/Head'
 import { useAuthenticationState } from 'utilities/firebase'
 import Spinner from 'components/Spinner'
 import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Profile = () => {
   const [user, loading, error] = useAuthenticationState()
   const router = useRouter()
-  if (!user) {
-    router.push('/')
-  }
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/')
+    }
+  }, [user, router])
+
   if (error) {
     alert(error.message)
   }
