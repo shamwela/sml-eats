ba1b2cb1379f51ca90d463873fb4ec47c43964f3
Sha Mwe La <shamwela2002@gmail.com>
2022-05-31 19:15:20 +0630

    Try to fix

--- pages/api/error.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { withSentry } from '@sentry/nextjs'
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  throw new Error('API throw error test')
-  res.status(200).json({ name: 'John Doe' })
-}
-
-export default withSentry(handler)
