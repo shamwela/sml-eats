16df2200d943bb94a65068ec13abd71cb88494e5
Sha Mwe La <shamwela2002@gmail.com>
2022-05-31 19:00:44 +0630

    Add Sentry

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,14 @@ const Home = ({
   return (
     <>
       <Head title='Order food online' />
+      <button
+        type='button'
+        onClick={() => {
+          throw new Error('Sentry Frontend Error')
+        }}
+      >
+        Throw error
+      </button>
       <h2>Explore by category</h2>
       <ItemContainer>
         {categories.map(
