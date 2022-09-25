9a8f514ac8573c7ee9c1a8eb25e02173248b77c5
Sha Mwe La <shamwela2002@gmail.com>
2022-09-25 11:10:43 +0630

    Rate limit the API

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -11,12 +11,21 @@ import categoryRouter from './routers/category'
 import restaurantRouter from './routers/restaurant'
 import { prisma } from './prisma/prismaClient'
 import type { RestaurantsOnUsers } from '@prisma/client'
+import { rateLimit } from 'express-rate-limit'
 
 const app = express()
 app.use(
   cors({
     origin: frontendUrl,
     credentials: true,
+  })
+)
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   })
 )
 app.use(xssClean())
