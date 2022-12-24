d6849cb3de407b005ed4d5ba8e842126734d5566
Sha Mwe La <shamwela2002@gmail.com>
2022-12-24 10:02:21 +0630

    Remove rate limit for now

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -20,14 +20,14 @@ app.use(
     credentials: true,
   })
 )
-app.use(
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  })
-)
+// app.use(
+//   rateLimit({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+//     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+//   })
+// )
 app.use(xssClean())
 app.use(helmet())
 app.use(express.json())
