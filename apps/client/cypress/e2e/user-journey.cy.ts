cf46fb4a6259689455854691321d35ad37a2a049
Sha Mwe La <shamwela2002@gmail.com>
2022-12-30 13:15:55 +0630

    Update Cypress

--- apps/client/cypress/e2e/user-journey.cy.ts
+++ apps/client/cypress/e2e/user-journey.cy.ts
@@ -1,19 +1,17 @@
-describe('User journey', () => {
-  it('tests user journey', () => {
-    cy.visit('/signin')
-    cy.get('[data-cy="email"]').type('shamwela@shamwela.com')
-    cy.get('[data-cy="password"]').type('password')
-    cy.get('[data-cy="sign-in-button"]').click()
-    cy.get('[alt="The Pizza Company"]').click()
-    const itemName = 'Seafood Cocktail'
-    cy.get(`[alt="${itemName}"]`).click()
-    cy.get('[aria-label="Increase quantity"]').click()
-    cy.get('[data-cy="order-button"]').click()
-    cy.get('[data-cy="cart-button"]').click()
-    // Since the quantity is increased once
-    cy.get('[data-cy="quantity-select"]').contains('2')
-    cy.get('[data-cy="item-name"]').should('contain.text', itemName)
-  })
+it('tests user journey', () => {
+  cy.visit('/signin')
+  cy.get('[data-cy="email"]').type('shamwela@shamwela.com')
+  cy.get('[data-cy="password"]').type('password')
+  cy.get('[data-cy="sign-in-button"]').click()
+  cy.get('[alt="The Pizza Company"]').click()
+  const itemName = 'Seafood Cocktail'
+  cy.get(`[alt="${itemName}"]`).click()
+  cy.get('[aria-label="Increase quantity"]').click()
+  cy.get('[data-cy="order-button"]').click()
+  cy.get('[data-cy="cart-button"]').click()
+  // Since the quantity is increased once, the quantity should be 2
+  cy.get('[data-cy="quantity-select"]').contains('2')
+  cy.get('[data-cy="item-name"]').should('contain.text', itemName)
 })
 
 export {}
